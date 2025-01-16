2872550c8830ef9e8c565590dd31183a70f3bfe0
MaxxPC <maxkanz1991@gmail.com>
2025-01-17 02:53:31 +0400

    change BasePage and SearchGroups

--- src/main/java/core/base/BasePage.java
+++ src/main/java/core/base/BasePage.java
@@ -1,23 +1,33 @@
 package core.base;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
 
-```public class BasePage {
+public class BasePage {
     // Общие элементы, которые используются на разных страницах
     protected SelenideElement headerLogo = $("[tsid='toolbar-search-input']");
-    protected SelenideElement searchField = $("[name='st.query']");
+    protected SelenideElement inputSearchField = $(".input__prt1l");
     protected SelenideElement vkServices = $("[data-l='t,vk_ecosystem']");
-    // Локатор для drop-down меню
-    private SelenideElement searchSelect = $(".dropdown-results__4nym2");
+    // Локатор для drop down меню
+    private SelenideElement dropDownMenu = $("[data-tsid='search-suggest-wrapper']");
 
     @Step("Выполянем поиск по сайту с запросом: {query}")
-    public void search(String query) {
-        searchField.shouldBe(visible).setValue(query);
+    public void setInputSearchField(String query) {
+        inputSearchField.shouldBe(visible).setValue(query);
+    }
+    @Step("Ожидание появления выпадающего меню с результатами")
+    public void setDropDownMenuVisible() {
+        dropDownMenu.shouldBe(Condition.visible).shouldHave(Condition.text("Тестировщик"));
+    }
+    @Step("Выбираем вкладку {тестировщик} из выпадающего списка и переходим на страницу со списком групп")
+    public void selectValue(String value) {
+        dropDownMenu.$(Selectors.byText(value)).click();
     }
     @Step ("Открываем VK services")
     public void openVkServices() {
@@ -27,16 +37,4 @@ import static com.codeborne.selenide.Selenide.*;
     public void clickLogo() {
         headerLogo.shouldBe(visible).click();
     }
-    @Step("Выбираем вкладку {value} из выпадающего списка")
-    public String searchSelectValue(String value) {
-        searchSelect.click();
-        SelenideElement valueItem = $(".suggest-list__zd7xg");
-        valueItem.scrollTo();
-        String qaText = valueItem.find(".suggest-item__zd7xg").text();
-        valueItem.click();
-        return qaText;
-    }
-}```
-
-// SelenideElement valueItem = $$(".suggest-list__zd7xg").get(3).$("a");
-//SelenideElement valueItem = $(String.format(".suggest-list__zd7xg", value));
+}
